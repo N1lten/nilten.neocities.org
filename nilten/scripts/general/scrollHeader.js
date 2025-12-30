f1d3fc3e794bf326a90dfb7193f1f21ad0e8b578
Nilten <miles.nilten@gmail.com>
2025-12-30 23:31:59 +0100

    + Added a quote from G4 to the scrolling header doohickey + Added "Under the Weather" to the artworks page ! (i really should start updating the layout of the art gallery at this point lmao)

--- nilten/scripts/general/scrollHeader.js
+++ nilten/scripts/general/scrollHeader.js
@@ -77,6 +77,7 @@ const headerText = [
     "\" It's all in the mind, y'know \"",
     "DON'T LOOK UP TRACK 18 OF THE WE LOVE KATAMARI SOUNDTRACK WORST MISTAKE OF MY LIFE",
     "\" Two of us riding nowhere~ ♪ Spending someones hard-earned pay~ ♪ You and me Sunday driving~ ♪ Not arriving~ ♪ On our way back home~ ♪ We're on our way home~ ♪ We're on our way home~ ♪ We're going home~ ♪ You and i have memories~ ♪ Longer than the road that stretches out ahead~ ♪ \"",
+    "\" I'M AT COMIC CON ! \"",
 ];
 
 const indexID = Math.floor(Math.random() * headerText.length);
