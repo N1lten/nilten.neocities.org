10f6637d5bb49c9c5ca52a4195af0ba8d2750e63
Nilten <miles.nilten@gmail.com>
2026-09-13 14:28:45 +0200

    updated the about header design

--- nilten/scripts/about/aboutHeaderRandomizer.js
+++ nilten/scripts/about/aboutHeaderRandomizer.js
@@ -11,18 +11,11 @@ var validURLs = [
     "url('images/about/headers/about_header_05.webp')",
     "url('images/about/headers/about_header_06.webp')",
     "url('images/about/headers/about_header_07.webp')",
-    "url('images/about/headers/about_header_08.webp')",
-    "url('images/about/headers/about_header_09.webp')",
-    "url('images/about/headers/about_header_11.webp')",
-    "url('images/about/headers/about_header_12.webp')",
-    "url('images/about/headers/about_header_13.webp')",
-    "url('images/about/headers/about_header_14.webp')",
 ]
 URLID = Math.floor(Math.random() * validURLs.length)
-//console.log(URLID);
+
 headerURL = validURLs[URLID];
-//console.log(headerURL);
+
 for(var i = 0; i < randClass.length; i++) {
     randClass.item(i).style.backgroundImage = headerURL;
 }
-//console.log(document.getElementById("aboutHeaderRandomizer").style.backgroundImage);
